acf56826c3487cb1ff1f33f1de9f598a0ac2e308
1113870631 <1113870631@qq.com>
2023-05-21 21:27:07 +0800

    css

--- ipcd3/main.cpp
+++ ipcd3/main.cpp
@@ -1,11 +1,15 @@
 #include "widget.h"
 
 #include <QApplication>
-
+#include <QFile>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Widget w;
+    QFile file(QString(":/Ubuntu.qss"));
+    file.open(QFile::ReadOnly);
+    QString css = QLatin1String(file.readAll());
+    w.setStyleSheet(css);
     w.show();
     return a.exec();
 }
